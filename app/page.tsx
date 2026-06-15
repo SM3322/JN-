f96a00532ea84f1379d81a1c877bc748c519a3d6
SM3322 <senad.mirtoski@gmail.com>
2026-06-15 21:43:14 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Бот за јавни набавки</h1>
+      <p>Веб-апликацијата е успешно поставена.</p>
+    </main>
+  );
+}
